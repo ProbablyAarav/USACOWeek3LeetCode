b1595ca074529ebfd2c8c7b6e19a8d694239921d
Aarav <aarav25803691@gmail.com>
2024-08-03 10:26:18 -0500

    Create palindromeNumber.cpp

---
+++ palindromeNumber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int original = x;
+        long long reversed = 0;
+        while (x > 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+        return original == reversed;
+    }
+};
